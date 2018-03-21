3e2524f379fc74b81003095d9fcac2e8f27566ff
Ivan Dejanovic <idejanovic@gmail.com>
2018-03-21 16:39:45 +0100

    Added BlockchainServiceImpl.

---
+++ src/main/java/com/quine/springbootblockchain/service/BlockchainServiceImpl.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.quine.springbootblockchain.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Ivan Dejanovic
+ *
+ */
+
+@Component
+@Scope("prototype")
+public class BlockchainServiceImpl implements Runnable {
+	@Value("${name}")
+	private String name;
+
+	@Override
+	public void run() {
+		System.out.println("Block operation started in app name: " + name);
+	}
+
+}
