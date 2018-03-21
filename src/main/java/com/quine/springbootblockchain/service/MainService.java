fe5c3657803fe92a4747ff3d43bb060203a644ad
Ivan Dejanovic <idejanovic@gmail.com>
2018-03-21 16:46:34 +0100

    Added initiate and block methods.

--- src/main/java/com/quine/springbootblockchain/service/MainService.java
+++ src/main/java/com/quine/springbootblockchain/service/MainService.java
@@ -15,4 +15,18 @@ public interface MainService {
 	 * @return greetings
 	 */
 	public String greetings();
+	
+	/**
+	 * Method that calls another system (set through JVM parameters) to initiate block addition to blockchain.
+	 * 
+	 * @return status
+	 */
+	public String initiate();
+	
+	/**
+	 * Method that starts (on another thread) creation of new block and start operation for adding it to blockchain.
+	 * 
+	 * @return status
+	 */
+	public String block();
 }
