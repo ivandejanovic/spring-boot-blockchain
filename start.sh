789efff68177acedfdb36f7a21d20d0439ac06fa
Ivan Dejanovic <idejanovic@gmail.com>
2018-03-21 16:39:22 +0100

    Added port values to all System instances.

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 java -Dname=System01 -Dport=8082 -jar target/spring-boot-blockchain-0.1.0.jar --server.port=8081&
-java -Dname=System02 -jar target/spring-boot-blockchain-0.1.0.jar --server.port=8082&
-java -Dname=System03 -jar target/spring-boot-blockchain-0.1.0.jar --server.port=8083&
+java -Dname=System02 -Dport=8083 -jar target/spring-boot-blockchain-0.1.0.jar --server.port=8082&
+java -Dname=System03 -Dport=8081 -jar target/spring-boot-blockchain-0.1.0.jar --server.port=8083&
