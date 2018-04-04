674a320d50d61f488d2d6f1da74277aef7e55860
Ivan Dejanovic <idejanovic@gmail.com>
2018-04-04 14:41:02 +0200

    Removed starting of third instance.

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 java -Dname=System01 -DnextPort=8082 -jar target/spring-boot-blockchain-0.1.0.jar --server.port=8081&
-java -Dname=System02 -DnextPort=8083 -jar target/spring-boot-blockchain-0.1.0.jar --server.port=8082&
-java -Dname=System03 -DnextPort=8081 -jar target/spring-boot-blockchain-0.1.0.jar --server.port=8083&
+java -Dname=System02 -DnextPort=8081 -jar target/spring-boot-blockchain-0.1.0.jar --server.port=8082&
